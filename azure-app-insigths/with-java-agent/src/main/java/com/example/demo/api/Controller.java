1dba779f60c411e97c5554f8bd37fff8ac635823
Artus Rocha <artusrocha@gmail.com>
2022-07-22 12:57:26 +0000

    organize code dirs

---
+++ azure-app-insigths/with-java-agent/src/main/java/com/example/demo/api/Controller.java
@@ -0,0 +1,32 @@
+package com.example.demo.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+// import ch.qos.logback.classic.Logger;
+// import ch.qos.logback.classic.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/api/v1")
+class Controller {
+
+    private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+
+    @GetMapping("/hello")
+    public String hello() {
+        logger.info("/hello request....");
+        return "Hello world!";
+    }
+
+    @GetMapping("/hello/{name}")
+    public String hello(@PathVariable() final String name) {
+        logger.info("/hello/{} request....", name);
+        return "Hello " + name + "!";
+    }
+
+}
